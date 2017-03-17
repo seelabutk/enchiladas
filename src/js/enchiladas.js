ced0c34de8d54d684fbd02d6f92a3b6ccf94dd75
Mohammad Raji <mahmadza@accona.eecs.utk.edu>
2017-03-17 17:30:04 -0500

    Added monkey testing

--- src/js/enchiladas.js
+++ src/js/enchiladas.js
@@ -28,6 +28,21 @@ $(document).ready(function(){
         img_tag_y = img_tag.offset().top;
     });
     
+  	function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    }; 
+
     function render(lowquality)
     {
         var m = $M(camera.Transform);
@@ -136,8 +151,14 @@ $(document).ready(function(){
         return false;
     });
 
-
     // First render
     img_tag.mousedown();
     img_tag.mouseup();
+
+	// Set up testing if needed
+	if (getUrlParameter("test"))
+    {
+        tester = new Tester();
+        tester.test();
+    }
 });         
