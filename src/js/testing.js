81caa3d53af1dd2c5870394221d7a7c3d8863d04
Mohammad Raji <moh853@gmail.com>
2017-03-28 07:54:04 -0500

    Extended testing.js to work with multiple hyperimages

--- src/js/testing.js
+++ src/js/testing.js
@@ -3,10 +3,11 @@ Tester = function(configs)
     horde = null;
     counter = 0;
     clickTypes = ['mousedown', 'mousemove', 'mouseup'];
-    var $el = $(".evrst");
-    prevX = $el.outerWidth() / 2.0;
-    prevY = $el.outerHeight() / 2.0;
+    this.element = $(configs.element);
+    prevX = this.element.outerWidth() / 2.0;
+    prevY = this.element.outerHeight() / 2.0;
     randy = function(){};
+    self = this;
     randy.pick = function()
     {
         var finish_move = Math.random();
@@ -14,8 +15,8 @@ Tester = function(configs)
         if (counter == 1 && finish_move < mouse_up_prob)
         {
             counter = 2;
-            prevX = $el.outerWidth() / 2.0;
-            prevY = $el.outerHeight() / 2.0;
+            prevX = self.element.outerWidth() / 2.0;
+            prevY = self.element.outerHeight() / 2.0;
             return 'mouseup';
         }
         else if (counter == 1 && finish_move >= mouse_up_prob)
@@ -39,20 +40,20 @@ Tester = function(configs)
     this.test = function()
     {
         var counter = 0;
+        self = this;
         var custom_toucher = gremlins.species.clicker()
             .clickTypes(['mousedown', 'mousemove', 'mouseup'])
             .canClick(function(element){
-                if (element.getAttribute("class") == "evrst")
+                if ($(element).hasClass("tapestry-test"))
                 {
                     return true;
                 }
                 return false;
             })
             .positionSelector(function(){
-                var $el = $(".evrst");
-                var offset = $el.offset();
-                newPos = [(prevX + Math.random() * 20) % $el.outerWidth() - 10.0, 
-                    (prevY + Math.random() * 20) % $el.outerHeight() - 10.0];
+                var offset = self.element.offset();
+                newPos = [(prevX + Math.random() * 20) % self.element.outerWidth() - 10.0, 
+                    (prevY + Math.random() * 20) % self.element.outerHeight() - 10.0];
                 prevX = newPos[0];
                 prevY = newPos[1];
 
@@ -64,9 +65,37 @@ Tester = function(configs)
             .gremlin(custom_toucher);
 
         horde.strategy(gremlins.strategies.distribution()
-                .delay(5)
+                .delay(20)
                 .distribution([0.33, 0.33, 0.33])
         );
         horde.unleash({nb: 10000});
     }
 }
+$(document).ready(function(){
+    
+  	function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    }; 
+
+	// Set up testing if needed
+	if (getUrlParameter("test"))
+    {
+        setTimeout(function(){
+            $(".tapestry-test").each(function(){
+                tester = new Tester({element: this});
+                tester.test();
+            });
+        }, 1000);
+    }
+});
