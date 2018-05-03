f338301d4338bb9b03c7243752b4f4340a5b7e8a
Mohammad Raji <moa@dragonfli.es>
2018-05-03 16:54:43 -0500

    changed lowquality to imagesize and fixed various forms of render calls

--- src/js/tapestry.js
+++ src/js/tapestry.js
@@ -78,7 +78,7 @@
         $(this.element).css("width", this.settings.width.toString() + "px");
         $(this.element).css("height", this.settings.height.toString() + "px");
 
-        if ($(this.element).attr("data-tiling"))
+        if (this.settings.n_tiles > 1)
         {
             this.setup_tiles();
         }
@@ -272,8 +272,13 @@
         return path;
     }
 
-    Tapestry.prototype.render = function(lowquality, remote_call, make_path_only)
+    Tapestry.prototype.render = function(imagesize, remote_call)
     {
+        if (typeof remote_call === 'undefined')
+        {
+            remote_call = false;
+        }
+
         var n_tiles = this.settings.n_tiles;
         var n_cols = Math.sqrt(n_tiles);
         var width = this.settings.width;
@@ -281,7 +286,7 @@
         var requests = [];
         for (var i = 0; i < n_tiles; i++)
         {
-            var path = this.make_request(lowquality, i);
+            var path = this.make_request(imagesize, i);
             var img = new Image();
             img.tileid = i.toString();
             var self = this;
@@ -291,15 +296,17 @@
             }
             img.src = path;
         }
-        /*
-        Promise.all(requests).then(function(results){
-            for (var i = 0; i < n_tiles; i++)
+
+        // Don't rotate linked views if this call is
+        // from one of them otherwise it'll be an infinite
+        // loop
+        if (!remote_call)
+        {
+            for (var i = 0; i < this.linked_objs.length; i++)
             {
-                $(this.element).children(".tapestry-tile-" + i.toString)
-                    .attr("src", results[i]);
+                this.linked_objs[i].render(imagesize, true);
             }
-        });
-        */
+        }
     }
 
     Tapestry.prototype.render_single = function(lowquality, remote_call)
@@ -382,13 +389,13 @@
         this.timelog = {};
     }
 
-    Tapestry.prototype.rotate = function(mouse_x, mouse_y, lowquality)
+    Tapestry.prototype.rotate = function(mouse_x, mouse_y, imagesize)
     {
         if (this.is_drag)
         {
             this.is_drag = false;
             this.camera.move(mouse_x, mouse_y);
-            this.render(lowquality);
+            this.render(imagesize);
             this.is_drag = true;
         }
     }
@@ -396,7 +403,7 @@
     Tapestry.prototype.unlink_camera = function()
     { 
         this.setup_camera();
-        this.render(0);
+        this.render(this.get_low_resolution());
     }
     
     Tapestry.prototype.link = function(target)
@@ -415,7 +422,7 @@
             // Add ourself to that object too
             target.linked_objs.push(this);
 
-            target.render(0);
+            target.render(this.get_low_resolution());
         }
     }
     
@@ -452,7 +459,7 @@
             var current_position = Vector.create(m.multiply(this.camera.position).elements.slice(0, 3));
             var pos = Vector.create([parseInt(position[0]), parseInt(position[1]), parseInt(position[2])]);
             this.camera.rotateTo(pos);
-            this.render(0);
+            this.render(this.get_low_resolution());
             return this;
         }
         else if (operation == 'link')
@@ -493,7 +500,7 @@
             targets = targets.replace(/\(|\)| /g, "");
             targets = targets.split(",");
             $(this.element).attr("data-dataset", targets[0]);
-            this.render(0);
+            this.render(this.get_low_resolution());
         }
 
     }
@@ -516,6 +523,20 @@
             }, 20);
             step += 0.05;
         }
+    }
+
+    Tapestry.prototype.get_high_resolution = function()
+    {
+        return this.settings.width;
+    }
+
+    Tapestry.prototype.get_low_resolution = function()
+    {
+        var intended = this.settings.width / 8;
+        // let's keep the min and max at 256 for now
+        var MIN_LOW_RES_SIZE = 256;
+        var MAX_LOW_RES_SIZE = 256;
+        return Math.min(Math.max(intended, MIN_LOW_RES_SIZE), MAX_LOW_RES_SIZE);
     }
 
     Tapestry.prototype.setup_handlers = function()
@@ -542,7 +563,7 @@
             {
                 var mouse_x = event.clientX - self.element.getBoundingClientRect().left;
                 var mouse_y = event.clientY - self.element.getBoundingClientRect().top;
-                self.rotate(mouse_x, mouse_y, 256); // Render low quality version
+                self.rotate(mouse_x, mouse_y, self.get_low_resolution()); // Render low quality version
             }
         });
 
@@ -584,11 +605,11 @@
 
                 self.camera.zoomScale -= delta;
                 self.camera.position.elements[2] = self.camera.zoomScale;
-                self.render(1);
+                self.render(self.get_low_resolution());
 
                 clearTimeout($.data(self, 'timer'));
                 $.data(self, 'timer', setTimeout(function() {
-                    self.render(0);
+                    self.render(self.get_high_resolution());
                 }, 500));
             }
             return false;
@@ -617,7 +638,7 @@
             console.log("touchend");
             self.is_drag = false;
 
-            self.render(0);
+            self.render(this.get_low_resolution());
             return false;
         });
 
@@ -644,6 +665,7 @@
         width: 512,
         height: 512,
         zoom: 512,
+        n_tiles: 1,
         max_cache_length: 512, // client-side caching for preventing browser request cancellation
         enable_zoom: true,
         enable_rotation: true,
